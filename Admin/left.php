04065f343198c08099268c4f5f1405dd8012782a
Faiz Tajwala <75023649+FaizTajwala@users.noreply.github.com>
2022-02-04 16:56:43 +1100

    Update left.php

--- Admin/left.php
+++ Admin/left.php
@@ -3,22 +3,6 @@
 <!DOCTYPE html >
 <html>
 <head>
-<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
-
-<link href="style.css" rel="stylesheet" type="text/css" />
-
-<link href="../css/bootstrap.css" rel='stylesheet' type='text/css'/>
-<link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!--js--> 
-<script src="js/jquery.min.js"></script>
-
-<!--/js-->
 <title>Around the world</title>
 </head>
 
